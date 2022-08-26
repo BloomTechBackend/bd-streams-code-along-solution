98a09f96c5fd5f6f377e3d8830e9cc235c44cd65
jcassidy91 <jamie@Lambda-School-Jamies-MacBook-Pro.local>
2022-08-25 20:12:46 -0700

    initial commit

---
+++ src/com/amazon/ata/streams/resources/Suit.java
@@ -0,0 +1,5 @@
+package com.amazon.ata.streams.resources;
+
+public enum Suit {
+    HEARTS, SPADES, DIAMONDS, CLUBS;
+}
---
+++ src/com/amazon/ata/streams/resources/UnknownCountryOfOriginException.java
@@ -0,0 +1,4 @@
+package com.amazon.ata.streams.resources;
+
+public class UnknownCountryOfOriginException extends Exception {
+}
